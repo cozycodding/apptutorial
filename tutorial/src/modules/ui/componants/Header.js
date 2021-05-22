bc58d98e5ab12c342b75c330602c2029d779a80f
pae <pae@paes-MBP.lan>
2021-05-22 21:14:02 +0700

    folder ui

---
+++ tutorial/src/modules/ui/componants/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {
+  AppBar,
+  Toolbar,
+  Link,
+  FormControlLabel,
+  IconButton,
+  Badge,
+} from '@material-ui/core'
+import { Link as RouterLink, useHistory } from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <AppBar>
+      <Toolbar>
+        <Link></Link>
+        <Link></Link>
+
+        <Link></Link>
+      </Toolbar>
+    </AppBar>
+  )
+}
